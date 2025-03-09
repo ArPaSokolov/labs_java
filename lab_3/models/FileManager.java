aa0dd1ab6a4b862c6758df952dd997499aad2038
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2025-03-10 00:57:58 +0200

    Add: Возможность добавлять фильмы, удалять их и обновлять список

--- lab_3/models/FileManager.java
+++ lab_3/models/FileManager.java
@@ -23,4 +23,57 @@ public class FileManager {
         }
         return movies;
     }
+
+    // Обновление списка фильмов (перезаписываем файл)
+    public static boolean updateMoviesList(List<Movie> movies) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(MOVIES_FILE))) {
+            for (Movie movie : movies) {
+                writer.println(movie.getMovieTitle() + ";" + movie.getLength());
+            }
+            System.out.println("Список обновлен!");
+            return true;
+        } catch (IOException e) {
+            System.out.println("Ошибка сохранения: " + e.getMessage());
+            return false;
+        }
+    }
+
+    // Добавление нового фильма
+    public static void addMovie(List<Movie> movies, Scanner scanner) {
+        System.out.println("\nДобавление нового фильма");
+        System.out.println("Введите название фильма: ");
+        String title = scanner.nextLine();
+        System.out.println("Введите длительность фильма (чч:мм): ");
+        String length = scanner.nextLine();
+        try (PrintWriter writer = new PrintWriter(new FileWriter(MOVIES_FILE))) {
+            movies.add(new Movie(title, length));
+            updateMoviesList(movies);            
+        } catch (IOException e) {
+            System.out.println("Ошибка сохранения: " + e.getMessage());
+        }
+    }
+
+    // Удаление фильма
+    public static void removeMovie(List<Movie> movies, Scanner scanner) {
+        if (movies.isEmpty()) {
+            System.out.println("Список фильмов пустой");
+            return;
+        }
+    
+        System.out.println("Список фильмов, доступных для удаления:");
+        for (int i = 1; i < movies.size(); i++) {
+            System.out.println(i + ". " + movies.get(i));
+        }
+
+        System.out.print("Введите номер фильма для удаления: ");
+        int index = scanner.nextInt();
+        if (index >= 0 && index < movies.size()) {
+            String deletedFilmName = movies.get(index).getMovieTitle();
+            movies.remove(index);
+            System.out.println("Удален фильм: " + deletedFilmName);
+            updateMoviesList(movies);
+        } else {
+            System.out.println("Некорректный номер!");
+        }
+    }
 }
